224d2f0a8f23e70a74a33fc3b8e6df41f1cfe56a
joly-nutella <slavik.riabchuk@gmail.com>
2020-03-24 23:27:50 +0200

    my first commit

---
+++ script.js
@@ -0,0 +1,18 @@
+console.log("Hello, World!");
+
+console.warn("hillel at 19:15");
+
+console.error("it's an error");
+
+console.time("Execution time took");
+console.timeEnd("Execution time took");
+
+console.group("Bridge");
+console.log("Bridge Officers");
+
+console.group("Engine");
+console.log("Engineers");
+
+console.group("Ratings");
+console.log("Sailors");
+console.groupEnd();
